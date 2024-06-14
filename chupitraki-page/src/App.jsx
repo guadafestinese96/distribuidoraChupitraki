f66de47dcfcb3ca64fa7c13572bdce173206d15b
guadafestinese96 <guadalupefestinese@gmail.com>
2024-06-13 21:39:28 -0300

    .

--- chupitraki-page/src/App.jsx
+++ chupitraki-page/src/App.jsx
@@ -2,16 +2,16 @@ import './App.css'
 import Index from './components/inicio/Inicio'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
+import Layout from './components/layout/layout'
 
 function App() {
-
-  return (
-      <div>
-        <Header/>
-        <Index/>
-        <Footer/>
-      </div>
-    )
+return(
+  <div>
+    <Layout>
+      <Index/>
+    </Layout>
+  </div>
+)
         
 }
 
--- chupitraki-page/src/components/itemDetail/itemDetail.jsx
+++ chupitraki-page/src/components/itemDetail/itemDetail.jsx
@@ -1,13 +1,11 @@
-import "./Item.css"
-
-
-export default function Item({item}){
+export default function ItemDetail(){
     return(
         <div className="itemContainer">
             <img className="itemImg" src={item.img} alt={item.modelo} />
             <p className="itemMarca">{item.marca}</p>
             <p className="itemModelo">{item.modelo}</p>
             <p className="itemPrecio">${item.precio}</p>
+            <button>-</button><span>0</span><button>+</button>
         </div>
     )
 }
